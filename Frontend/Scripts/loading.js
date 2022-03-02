f11daf2c6e5487e536c221dba8d34d12deaec94e
Anupam Maurya <96181413+anupammaurya6767@users.noreply.github.com>
2022-03-02 20:56:34 +0530

    Add files via upload

---
+++ Frontend/Scripts/loading.js
@@ -0,0 +1,9 @@
+
+  function load2(){
+    setTimeout(() => {
+      $('#preloader').addClass('loaded');
+      $('#container').addClass('.dd');
+    }, 3000);
+  }
+
+  // document.getElementById("btn2-div").addEventListener('click',load2);
